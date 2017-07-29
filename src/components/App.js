0464035dc35b7e61c8c47bcc73ac4a69a0ad8eea
Trai Compton <tlcompton2546@outlook.com>
2017-07-29 01:47:05 +0000

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,30 @@ import React from 'react';
 
 import FruitBasket from './FruitBasket';
 
-const App = () => <FruitBasket />;
+class App extends React.Component {
+    constructor(){
+        super();
+        
+        this.state = {
+            fruit: [],
+            filters: [],
+            currentFilter: null
+        };
+        
+        this.handleChange = this.handleChange.bind(this);
+    }
+    
+    handleChange(e){
+        this.setState({
+            filter: e.target.value
+        })
+    }
+    
+    render(){
+        return(
+            <FruitBasket />    
+        )
+    }
+}
 
 export default App;
